5d4536876d4be388571b946220276e91450dd96c
costia123 <78858057+costia123@users.noreply.github.com>
2022-05-20 15:59:31 +0200

    update visual

--- js/test.js
+++ js/test.js
@@ -1,11 +1,17 @@
 function test(){
     console.log("first")
-    axios.get(`https://api.wsf.fr/rss.xml`).then((res) => {
+    axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+        headers: {
+           "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          'X-CMC_PRO_API_KEY': '307ff52f-acda-4891-9c37-68cb910c20c1',
+        },
+      }).then((res) => {
         console.log(res)
     })
     var l = "le prix est dynamic"
     return l;
 }
+
 function test2() {
     console.log("firstss")
     var l = "le prix est dynamic et reload !!!"
